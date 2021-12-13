9a85c84b9b857c0cf7f6de91118b3a38d3da36d9
Moe <muhammadcodez@gmail.com>
2021-12-13 23:23:43 +0200

    Koii.X rework

--- src/services/hooks/index.js
+++ src/services/hooks/index.js
@@ -1,3 +1,2 @@
-export { default as useFinnie } from "./useFinnie";
 export { default as useSdk } from "./useSdk";
 export { default as useKoii } from "./useKoii";
--- src/services/hooks/useSdk.js
+++ src/services/hooks/useSdk.js
@@ -1,24 +1,26 @@
 import React from "react";
+// context
+import { useFinnie } from "components/context/finnie";
+// services
 import { getNftsStats } from "services/utils";
 // sdk
 import * as kweb from "@_koi/sdk/web";
 let koiSDK = new kweb.Web();
-// hooks
-import { useFinnie } from "services/hooks";
 
 export default function useSdk() {
   const [{ wallet, status, error }, setState] = React.useState({ status: "idle", error: null });
   /* useFinnie to connect to finnie */
-  const { connectFinnie } = useFinnie();
+  const {
+    state: { connectFinnie }
+  } = useFinnie();
 
   const getKoiiNfts = async () => {
     setState(prevState => ({ ...prevState, status: "loading", error: null }));
     try {
       const walletAddress = await connectFinnie();
-      console.log({ walletAddress });
       const nfts = await koiSDK.getNftsByOwner(walletAddress);
       const [totalAttention, totalReward] = getNftsStats(nfts);
-      console.log({ nfts, totalAttention, totalReward });
+
       setState(prevState => ({ ...prevState, status: "success", wallet: { nfts, totalAttention, totalReward }, error: null }));
     } catch (error) {
       console.log({ error });
