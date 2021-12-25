e896e64bc0dc1d31ef37c8406fc44ff23338e3ff
Moe <muhammadcodez@gmail.com>
2021-12-25 02:10:29 +0200

    full fledged pagination

--- src/components/sections/TopNftsContent.tsx
+++ src/components/sections/TopNftsContent.tsx
@@ -1,3 +1,4 @@
+import useInfiniteScroll from "react-infinite-scroll-hook";
 import { useEffect, useState } from "react";
 import { StringParam, withQueryParams } from "use-query-params";
 // api
@@ -5,37 +6,54 @@ import { useNfts } from "hooks/api";
 // ui
 import { Center, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { NftFeaturedCard } from "components/cards";
+import { EmptyState } from "components/ui";
 
 const TopNftsContent = ({ query }: any) => {
   /*  */
   const { t: timeframe } = query;
   /* Get nfts based on the timeframe */
-  const { data: nfts, isLoading } = useNfts({ timeframe });
-  // const [nftsView, setNftsView] = useState(nfts);
+  const { data: nfts, isLoading, isError } = useNfts({ timeframe });
 
-  // useEffect(() => {
-  //   setNftsView(nfts);
-  // }, [nfts]);
-  // // /* Pagination  */
-  // console.log({ nfts });
+  /* Pagination */
+  const [nftsView, setNftsView] = useState(nfts);
+  const [page, setPage] = useState(0);
+  /* Get 12 more every scroll */
+  useEffect(() => {
+    setNftsView(nfts?.slice?.(0, page === 0 ? 12 : page * 12));
+  }, [nfts, page]);
+  /* Reset page index when changing timeframe */
+  useEffect(() => {
+    setPage(0);
+  }, [timeframe]);
+
+  const hasMore = nftsView?.length !== nfts?.length;
+
+  const [sentryRef] = useInfiniteScroll({
+    loading: false,
+    hasNextPage: hasMore,
+    onLoadMore: () => {
+      setPage(page => page + 1);
+    },
+    disabled: isError,
+    rootMargin: "0px 0px 0px 0px"
+  });
 
   return (
     <>
       {isLoading && (
-        <Center w="100%" minH="300px">
+        <Center w="100%" minH={{ base: "300px", md: "600px" }}>
           <Spinner thickness="3px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="lg" />
         </Center>
       )}
+      {/* Loading | Nfts */}
       {!isLoading && (
         <SimpleGrid w="100%" minW="0" minH="0" gap="8" columns={[1, 2, 4]}>
-          {nfts?.map((nft: Record<string, any>) => (
-            <NftFeaturedCard nft={nft} key={nft?.id} />
-          ))}
-          {/* No nfts to show. */}
-          {/* {nftsView?.length === 0 && <Center w="100%" minH="300px">
-            </Center>} */}
+          {nftsView?.length !== 0 && nftsView?.map((nft: Record<string, any>) => <NftFeaturedCard nft={nft} key={nft?.id} />)}
         </SimpleGrid>
       )}
+      {hasMore && <div ref={sentryRef} />}
+      {/* No nfts to show. */}
+      {!isLoading && nftsView?.length === 0 && <EmptyState minH={{ base: "300px", md: "600px" }} />}
     </>
   );
 };
